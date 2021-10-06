07b59ee8f7d3eedc17b935bf7a6512ab0881b833
Claudio Lucena <83517071+claudi0s0uza@users.noreply.github.com>
2021-10-06 16:23:40 -0300

    Update monitora.sh

--- monitora.sh
+++ monitora.sh
@@ -1,13 +1,10 @@
 #!bin/bash
 
-#DATE=$(date +%H:%M)
 LOG=/app/log-$(hostname).txt
 NOME=$(hostname)
-
 
 while :; do
 
         sleep 3
         echo "Esse LOG vem do POD - $NOME -------- $(date)" >> $LOG
-
 done
